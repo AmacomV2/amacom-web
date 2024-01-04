24ff2307ac5e8cbf14f198c92513bbcfa95d3017
VEEB <nenitq@gmail.com>
2024-01-03 23:12:18 -0500

    [Dashboard]: avance dashboard enfermería

--- source/main/src/app/doctor/dashboard/dashboard.component.ts
+++ source/main/src/app/doctor/dashboard/dashboard.component.ts
@@ -57,250 +57,52 @@ export type radialChartOptions = {
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  @ViewChild('chart')
-  chart!: ChartComponent;
-  public areaChartOptions!: Partial<areaChartOptions>;
-  public radialChartOptions!: Partial<radialChartOptions>;
-  public linechartOptions!: Partial<linechartOptions>;
+  public listaSituaciones: Array<any> = [];
+  public listaEnfermeria: Array<any> = [];
+  public listaBitacora: Array<any> = [];
+  public listaPacientes: Array<any> = [];
+  public indicePrimerItem: number = 1;
+  public indiceUltimoItem: number = 10;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
-
-  // TODO start
-  tasks = [
-    {
-      id: '1',
-      title: 'Check patient report',
-      done: true,
-      priority: 'High',
-    },
-    {
-      id: '2',
-      title: 'Request for festivle holiday',
-      done: false,
-      priority: 'High',
-    },
-    {
-      id: '3',
-      title: 'Order new medicine stock',
-      done: false,
-      priority: 'Low',
-    },
-    {
-      id: '4',
-      title: 'Remind for lunch in hotel',
-      done: true,
-      priority: 'Normal',
-    },
-    {
-      id: '5',
-      title: 'Conference in london',
-      done: false,
-      priority: 'High',
-    },
-    {
-      id: '6',
-      title: 'Announcement for',
-      done: false,
-      priority: 'Normal',
-    },
-    {
-      id: '7',
-      title: 'call bus driver',
-      done: true,
-      priority: 'High',
-    },
-    {
-      id: '8',
-      title: 'Web service data load issue',
-      done: false,
-      priority: 'High',
-    },
-    {
-      id: '9',
-      title: 'Java compile error',
-      done: false,
-      priority: 'Low',
-    },
-    {
-      id: '10',
-      title: 'Integrate project with spring boot',
-      done: true,
-      priority: 'High',
-    },
-  ];
-
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
-  }
-
-  toggle(task: { done: boolean }) {
-    task.done = !task.done;
-  }
-  // TODO end
-
   ngOnInit() {
-    this.chart1();
-    this.chart2();
-    this.chart3();
+    this.llenarLista();
   }
-  private chart1() {
-    this.areaChartOptions = {
-      series: [
-        {
-          name: 'New Patients',
-          data: [31, 40, 28, 51, 42, 85, 77],
-        },
-        {
-          name: 'Old Patients',
-          data: [11, 32, 45, 32, 34, 52, 41],
-        },
-      ],
-      chart: {
-        height: 350,
-        type: 'area',
-        toolbar: {
-          show: false,
-        },
-        foreColor: '#9aa0ac',
-      },
-      colors: ['#7D4988', '#66BB6A'],
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: 'smooth',
-      },
-      grid: {
-        show: true,
-        borderColor: '#9aa0ac',
-        strokeDashArray: 1,
-      },
-      xaxis: {
-        type: 'datetime',
-        categories: [
-          '2018-09-19T00:00:00.000Z',
-          '2018-09-19T01:30:00.000Z',
-          '2018-09-19T02:30:00.000Z',
-          '2018-09-19T03:30:00.000Z',
-          '2018-09-19T04:30:00.000Z',
-          '2018-09-19T05:30:00.000Z',
-          '2018-09-19T06:30:00.000Z',
-        ],
-      },
-      legend: {
-        show: true,
-        position: 'top',
-        horizontalAlign: 'center',
-        offsetX: 0,
-        offsetY: 0,
-      },
+  llenarLista(){
+    this.listaSituaciones = [
+     {id:1, tema:"tema 2", date:"27/06/2023", descripcion:"me siento triste", gradoAfectacion: 8, sentimiento: "tristeza", firstPen: "Me siento solo", comportamiento: "solitario"},
+     {id:2, tema:"tema 2", date:"27/06/2023", descripcion:"me siento feliz", gradoAfectacion: 1, sentimiento: "felicidad", firstPen: "Soy feliz", comportamiento: "alegre"},
+     {id:3, tema:"tema 3", date:"27/06/2023", descripcion:"me duele la panza", gradoAfectacion: 4, sentimiento: "debilidad", firstPen: "Estoy enferma", comportamiento: "debilidad"},
+     {id:4, tema:"tema 4", date:"27/06/2023", descripcion:"no me duele la panza", gradoAfectacion: 0, sentimiento: "neutral", firstPen: "nada", comportamiento: "ninguno"}
+    ];
 
-      tooltip: {
-        theme: 'dark',
-        marker: {
-          show: true,
-        },
-        x: {
-          format: 'dd/MM/yy HH:mm',
-        },
-      },
-    };
-  }
-  private chart2() {
-    this.radialChartOptions = {
-      series: [44, 55, 67],
-      chart: {
-        height: 265,
-        type: 'radialBar',
-      },
-      plotOptions: {
-        radialBar: {
-          dataLabels: {
-            name: {
-              fontSize: '22px',
-            },
-            value: {
-              fontSize: '16px',
-            },
-            total: {
-              show: true,
-              label: 'Total',
-              formatter: function () {
-                return '249';
-              },
-            },
-          },
-        },
-      },
-      colors: ['#ffc107', '#3f51b5', '#8bc34a'],
+    this.listaEnfermeria = [
+      {id:1, nombre:"Esmeralda", estado:"activo"},
+      {id:2, nombre:"Coral", estado:"activo"},
+      {id:3, nombre:"Alicia", estado:"activo"},
+      {id:4, nombre:"Roberto", estado:"inactivo"},
+      {id:5, nombre:"Sum", estado:"inactivo"}
+     ];
 
-      labels: ['Face TO Face', 'E-Consult', 'Available'],
-    };
+     this.listaBitacora = [
+      {id:1, i_name:"bitacora 1", date:"27/06/2023", descripcion:"primera bitacora"},
+      {id:2, i_name:"bitacora 2", date:"27/06/2023", descripcion:"segunda bitacora"},
+      {id:3, i_name:"bitacora 3", date:"27/06/2023", descripcion:"tercera bitacora"}
+     ];
+
+     this.listaPacientes = [
+      {id:1, nombre:"Magenta", estado:"activo"},
+      {id:2, nombre:"Perla", estado:"activo"},
+      {id:3, nombre:"Victoria", estado:"activo"},
+      {id:4, nombre:"Rene", estado:"inactivo"}
+     ];
   }
-  private chart3() {
-    this.linechartOptions = {
-      series: [
-        {
-          name: 'Male',
-          data: [44, 55, 57, 56, 61, 58],
-        },
-        {
-          name: 'Female',
-          data: [76, 85, 101, 98, 87, 105],
-        },
-      ],
-      chart: {
-        type: 'bar',
-        height: 350,
-        dropShadow: {
-          enabled: true,
-          color: '#000',
-          top: 18,
-          left: 7,
-          blur: 10,
-          opacity: 0.2,
-        },
-        toolbar: {
-          show: false,
-        },
-        foreColor: '#9aa0ac',
-      },
-      colors: ['#5C9FFB', '#AEAEAE'],
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          columnWidth: '55%',
-          borderRadius: 5,
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      grid: {
-        show: true,
-        borderColor: '#9aa0ac',
-        strokeDashArray: 1,
-      },
-      stroke: {
-        show: true,
-        width: 2,
-        colors: ['transparent'],
-      },
-      xaxis: {
-        categories: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-      },
-      yaxis: {},
-      fill: {
-        opacity: 1,
-      },
-      tooltip: {
-        theme: 'dark',
-        marker: {
-          show: true,
-        },
-        x: {
-          show: true,
-        },
-      },
-    };
+
+  searchSituacion(data: any){
+    //temporal
+  }
+
+  searchBitacora(data: any){
+    //temporal
   }
 }
