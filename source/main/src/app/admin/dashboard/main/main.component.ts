062d0de1e1d9e9f1dadcb4f2be0cdc5b8c996911
VEEB <nenitq@gmail.com>
2024-01-08 23:08:51 -0500

    [Dashboard]: arreglos

--- source/main/src/app/admin/dashboard/main/main.component.ts
+++ source/main/src/app/admin/dashboard/main/main.component.ts
@@ -49,29 +49,21 @@ export class MainComponent implements OnInit {
     this.listaSituaciones = [
      {id:1, tema:"tema 2", date:"27/06/2023", descripcion:"me siento triste", gradoAfectacion: 8, sentimiento: "tristeza", firstPen: "Me siento solo", comportamiento: "solitario"},
      {id:2, tema:"tema 2", date:"27/06/2023", descripcion:"me siento feliz", gradoAfectacion: 1, sentimiento: "felicidad", firstPen: "Soy feliz", comportamiento: "alegre"},
-     {id:3, tema:"tema 3", date:"27/06/2023", descripcion:"me duele la panza", gradoAfectacion: 4, sentimiento: "debilidad", firstPen: "Estoy enferma", comportamiento: "debilidad"},
-     {id:4, tema:"tema 4", date:"27/06/2023", descripcion:"no me duele la panza", gradoAfectacion: 0, sentimiento: "neutral", firstPen: "nada", comportamiento: "ninguno"}
+     {id:3, tema:"tema 3", date:"27/06/2023", descripcion:"me duele la panza", gradoAfectacion: 4, sentimiento: "debilidad", firstPen: "Estoy enferma", comportamiento: "debilidad"}
     ];
 
     this.listaEnfermeria = [
-      {id:1, nombre:"Esmeralda", estado:"activo"},
-      {id:2, nombre:"Coral", estado:"activo"},
-      {id:3, nombre:"Alicia", estado:"activo"},
-      {id:4, nombre:"Roberto", estado:"inactivo"},
-      {id:5, nombre:"Sum", estado:"inactivo"}
+      {id:1, nombre:"mario", estado:"activo"}
      ];
 
      this.listaBitacora = [
       {id:1, i_name:"bitacora 1", date:"27/06/2023", descripcion:"primera bitacora"},
-      {id:2, i_name:"bitacora 2", date:"27/06/2023", descripcion:"segunda bitacora"},
-      {id:3, i_name:"bitacora 3", date:"27/06/2023", descripcion:"tercera bitacora"}
+      {id:2, i_name:"bitacora 2", date:"27/06/2023", descripcion:"segunda bitacora"}
      ];
 
      this.listaPacientes = [
-      {id:1, nombre:"Magenta", estado:"activo"},
-      {id:2, nombre:"Perla", estado:"activo"},
-      {id:3, nombre:"Victoria", estado:"activo"},
-      {id:4, nombre:"Rene", estado:"inactivo"}
+      {id:1, nombre:"may", estado:"activo"},
+      {id:2, nombre:"pruebita", estado:"activo"}
      ];
   }
 
